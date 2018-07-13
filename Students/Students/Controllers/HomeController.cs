35d637b2b9cf40d56b52b68de94caec0d4fdb74e
Budya <Budya2012@gmail.com>
2018-07-13 14:15:49 +0300

    Students finish

--- Students/Students/Controllers/HomeController.cs
+++ Students/Students/Controllers/HomeController.cs
@@ -1,30 +1,72 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Students.Models;
 
 namespace Students.Controllers
 {
     public class HomeController : Controller
     {
+        private StudentsContext db = new StudentsContext();
         public ActionResult Index()
         {
-            return View();
+            return View(db.Students.ToList());
         }
 
-        public ActionResult About()
+        public ActionResult Details(int id = 0)
         {
-            ViewBag.Message = "Your application description page.";
+            Student student = db.Students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
 
-            return View();
+            return View(student);
         }
 
-        public ActionResult Contact()
+        public ActionResult Edit(int id = 0)
         {
-            ViewBag.Message = "Your contact page.";
+            Student student = db.Students.Find(id);
 
-            return View();
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.Courses = db.Courses.ToList();
+            return View(student);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Student student, int[] selectedCourses)
+        {
+            Student newStudent = db.Students.Find(student.Id);
+            newStudent.Name = student.Name;
+            newStudent.Surname = student.Surname;
+
+            newStudent.Courses.Clear();
+            if (selectedCourses != null)
+            {
+                //Получаем выбранные курсы
+                foreach (var c in db.Courses.Where(co=>selectedCourses.Contains(co.Id)))
+                {
+                    newStudent.Courses.Add(c);
+                }
+            }
+            db.Entry(newStudent).State = EntityState.Modified;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
         }
     }
 }
--- Students/Students/Global.asax.cs
+++ Students/Students/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using Students.Models;
 
 namespace Students
 {
@@ -12,6 +14,8 @@ namespace Students
     {
         protected void Application_Start()
         {
+            Database.SetInitializer(new CourseDbInitializer());
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
