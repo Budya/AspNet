02b6c024322fd99308a888958369a9b0dae071f2
Budya <Budya2012@gmail.com>
2018-07-13 11:52:00 +0300

    Students

---
+++ Students/Students/Models/StudentContext.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Students.Models
+{
+    public class StudentContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Course> Courses { get; set; }
+
+        public StudentContext(): base("DefaultConnection") { }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Course>().HasMany(c => c.Students)
+                .WithMany(s => s.Courses)
+                .Map(t => t.MapLeftKey("CourseId")
+                    .MapRightKey("StudentId")
+                    .ToTable("CourseStudent"));
+        }
+    }
+}
