ebc201f5d5c85bd57c48f6c73501f388ff098f56
Budya <Budya2012@gmail.com>
2018-07-16 16:18:51 +0300

    Team Filter

--- Football/Football/Controllers/HomeController.cs
+++ Football/Football/Controllers/HomeController.cs
@@ -19,6 +19,39 @@ namespace Football.Controllers
             return View(players.ToList());
         }
 
+        public ActionResult IndexFilter(int? team, string position)
+        {
+            IQueryable<Player> players = db.Players.Include(p => p.Team);
+            if (team != null && team != 0)
+            {
+                players = players.Where(p => p.TeamId == team);
+            }
+
+            if (!String.IsNullOrEmpty(position) && !position.Equals("Все"))
+            {
+                players = players.Where(p => p.Position == position);
+            }
+
+            List<Team> teams = db.Teams.ToList();
+            // Установка начального элемента, который позволит выбрать всех
+            teams.Insert(0, new Team {Name = "Все", Id = 0});
+
+            PlayerListViewModel plvm = new PlayerListViewModel
+            {
+                Players = players.ToList(),
+                Teams = new SelectList(teams, "Id", "Name"),
+                Positions = new SelectList(new List<String>()
+                {
+                    "Все",
+                    "Нападающий",
+                    "Полузащитник",
+                    "Защитник",
+                    "Вратарь"
+                })
+            };
+            return View(plvm);
+        }
+
         public ActionResult TeamDetails(int? id)
         {
             if (id == null)
---
+++ Football/Football/Models/PlayerListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Football.Models
+{
+    public class PlayerListViewModel
+    {
+        public IEnumerable<Player> Players { get; set; }
+        public SelectList Teams { get; set; }
+        public SelectList Positions { get; set; }
+    }
+}
