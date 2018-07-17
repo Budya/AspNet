160b406070cd1a0d8d37de2863611d40fff3f234
Budya <Budya2012@gmail.com>
2018-07-17 14:18:45 +0300

    Filters

---
+++ AutorizFilterApp/AutorizFilterApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="AutorizFilterApp.MvcApplication" Language="C#" %>
---
+++ ValidationApp/ValidationApp/Models/Book.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ValidationApp.Models
+{
+    public class Book
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть заполнено")]
+        [Remote("CheckName", "Book", ErrorMessage = "Недопустимое имя книги")]
+        [Display(Name = "Название")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть заполнено")]
+        [Display(Name = "Автор")]
+        public string Author { get; set; }
+
+        [Required(ErrorMessage = "Поле должно быть заполнено")]
+        [Display(Name = "Год")]
+        public int Year { get; set; }
+    }
+}
