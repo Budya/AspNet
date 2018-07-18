7dc35f541bdf6776ad5ca15033edd84eed769fed
Budya <Budya2012@gmail.com>
2018-07-18 17:04:17 +0300

    123

--- AutorizFilterApp/AutorizFilterApp/Controllers/HomeController.cs
+++ AutorizFilterApp/AutorizFilterApp/Controllers/HomeController.cs
@@ -3,20 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutorizFilterApp.Filters;
 
 namespace AutorizFilterApp.Controllers
 {
     public class HomeController : Controller
     {
+        [MyAuthAttribute]
         public ActionResult Index()
         {
             return View();
         }
 
+       // [HandleError(ExceptionType = typeof(System.IndexOutOfRangeException), View = "ExceptionFound")]
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
-
+          //  int[] mas = new int[2];
+            //mas[6] = 4;
+            
             return View();
         }
 
