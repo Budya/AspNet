9a43e2d32e7c46310ce5dbd119ddd92b2cf8ddf1
Budya <Budya2012@gmail.com>
2018-07-20 15:09:51 +0300

    ChatFinish

---
+++ ChatAspNet/ChatAspNet/Models/ChatModel.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Remoting.Lifetime;
+using System.Security.Policy;
+using System.Web;
+
+namespace ChatAspNet.Models
+{
+    public class ChatModel
+    {
+        public List<ChatUser> Users; //All chat users
+
+        public List<ChatMessage> Messages; // Chat messages
+
+        public ChatModel()
+        {
+            Users = new List<ChatUser>();
+            Messages = new List<ChatMessage>();
+
+            Messages.Add(new ChatMessage()
+            {
+                Text = "Чат запущен "+ DateTime.Now
+            });
+        }
+    }
+
+    public class ChatUser
+    {
+        public string Name;
+        public DateTime LoginTime;
+        public DateTime LastPing;
+    }
+
+    public class ChatMessage
+    {
+        // Author of message, if null - server is author
+        public ChatUser User;
+        // Time of message
+        public DateTime Date = DateTime.Now;
+        // text of message
+        public string Text = "";
+    }
+}
