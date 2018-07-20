6cfa8fe513daaeb9fbc72f1944b7d551ba0c9561
Budya <Budya2012@gmail.com>
2018-07-20 16:27:01 +0300

    OWIN

---
+++ KatanaApp/KatanaApp/Startup1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(KatanaApp.Startup1))]
+
+namespace KatanaApp
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Run(context =>
+            {
+                context.Response.ContentType = "text/html; charset=utf-8";
+                return context.Response.WriteAsync("<h2>Привет Мир!</h2>");
+            });
+        }
+    }
+}
